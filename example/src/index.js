496f5040907798f858c3282e8f62fd8ddd964f3c
Eran Geva <eran.geva@gmail.com>
2022-10-01 20:55:04 +0300

    advance to react 18 and use createRoot

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,5 @@
-import './index.css'
-
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+createRoot(document.getElementById('root')).render(<App />)
