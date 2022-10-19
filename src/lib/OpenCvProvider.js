8a460b0365c99cb06415d129dbe6b7bdd6ccd075
Eran Geva <eran.geva@gmail.com>
2022-10-19 23:46:16 +0300

    revert to using moduleConfig

--- src/lib/OpenCvProvider.js
+++ src/lib/OpenCvProvider.js
@@ -6,27 +6,29 @@ const { Consumer: OpenCvConsumer, Provider } = OpenCvContext
 
 export { OpenCvConsumer, OpenCvContext }
 
+const scriptId = 'opencv-react'
+
+const moduleConfig = {
+  wasmBinaryFile: 'opencv_js.wasm',
+  usingWasm: true
+}
+
 export const OpenCvProvider = ({ openCvVersion = '4.5.5', openCvPath = '', children }) => {
   const [cvInstance, setCvInstance] = React.useState({ loaded: false, cv: undefined });
 
   React.useEffect(() => {
-    const scriptId = 'opencv-react'
     if (document.getElementById(scriptId) || window.cv) {
       return
-    }
-
-    window.Module = window.Module || {
-      wasmBinaryFile: 'opencv_js.wasm',
-      usingWasm: true,
-      onRuntimeInitialized: () => {
-        console.log('opencv loaded', window.cv)
-        setCvInstance({ loaded: true, cv: window.cv });
-      }
     }
 
     // https://docs.opencv.org/3.4/dc/de6/tutorial_js_nodejs.html
     // https://medium.com/code-divoire/integrating-opencv-js-with-an-angular-application-20ae11c7e217
     // https://stackoverflow.com/questions/56671436/cv-mat-is-not-a-constructor-opencv
+    moduleConfig.onRuntimeInitialized = () => {
+      setCvInstance({ loaded: true, cv: window.cv });
+    }
+    window.Module = moduleConfig
+
     const scriptElement = document.createElement('script')
     scriptElement.id = scriptId
     scriptElement.nonce = true
