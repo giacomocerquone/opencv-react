918092c506b3b8df3e17c4488d182aa3d361c0c8
giacomocerquone <cerquone96@hotmail.it>
2023-01-19 22:46:00 +0100

    fix: example

--- example/src/index.js
+++ example/src/index.js
@@ -2,6 +2,4 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-// React 18 uses createRoot
-// https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
 createRoot(document.getElementById('root')).render(<App />)
